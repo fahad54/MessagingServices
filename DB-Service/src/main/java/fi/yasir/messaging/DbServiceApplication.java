9ca72120fda3bc244cbf510d3d518647095070be
Yasir Ali <yasirali1234@gmail.com>
2018-09-09 22:07:47 +0300

    project setup

---
+++ DB-Service/src/main/java/fi/yasir/messaging/DbServiceApplication.java
@@ -0,0 +1,15 @@
+package fi.yasir.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"fi.yasir"})
+public class DbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServiceApplication.class, args);
+	}
+}
